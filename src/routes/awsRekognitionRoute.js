02c0d8bfaedfbe00a6e1b5e13093580a9b82ffda
Kenny W. Lino <kennywlino@gmail.com>
2023-02-17 19:02:34 -0800

    Set up function to load images via fs.

--- src/routes/awsRekognitionRoute.js
+++ src/routes/awsRekognitionRoute.js
@@ -1,46 +1,46 @@
+import fs from 'fs';
+
 // import { RekognitionClient, DetectLabelsCommand } from "@aws-sdk/client-rekognition";
 
 // const client = new RekognitionClient({ region: "us-west-2"});
 
-// (curr) input: URL in string format
-// (goal) input: URL or image in any format (e.g. .jpg, .png, .gif, etc)
-// output: base-64 format image
-const processImage = async (imgUrl) => {
-  const reader = new FileReader();
-  try {
-    const response = await fetch(imgUrl);
-    const data = await response.arrayBuffer();
-    reader.readAsArrayBuffer(new Blob([data]));
-  } catch (error) {
-    console.error(error);
-  }
+// (curr) input: image path as string
+// output: ArrayBuffer
+const load = (imgPath) => {
+  fs.readFile(imgPath, (err, data) => {
+    if (err) {
+      throw err;
+    }
 
-  reader.onload = () => {
-    const arrayBuffer = reader.result;
-    console.log('arrayBuffer:', arrayBuffer);
-  };
+    // gives us a Buffer needed for AWS;
+    // AWS wants a Uint8Array and "Buffer instances are also JavaScript Uint8Array and TypedArray instances"
+    // https://nodejs.org/api/buffer.html#buffer
+    const buffer = Buffer.from(data, 'utf-8');
+    console.log('buffer inside load', buffer);
+    return buffer;
+  });
 };
 
 // input: image in base-64 format
 // output: labels from AWS Rekognition
-// const getLabels = async (img: Array<number>): Promise<any> => {
-//   const params = {
-//     Image: {
-//       Bytes: img,
-//     }
-//   };
+const getLabels = async (imgBuffer) => {
+  const params = {
+    Image: {
+      Bytes: imgBuffer,
+    }
+  };
 
-//   const command = new DetectLabelsCommand(params);
+  const command = new DetectLabelsCommand(params);
 
-//   try {
-//     const data = await client.send(command);
-//     return data;
-//   } catch (error: any) {
-//     console.error("Error:", error);
-//   }
-// };
+  try {
+    const data = await client.send(command);
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
 
 const path = "/Users/kennywlino/Downloads/test.jpeg";
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-processImage(path);
+let testBuffer = load(path);
+console.log('testBuffer:', testBuffer);
