b6549d9652d67ce8c2d8f8f5fb698a82851de2e6
Kenny W. Lino <kennywlino@gmail.com>
2023-02-17 17:55:27 -0800

    Removed references to TypeScript and switched to plain NodeJS for faster development.

---
+++ index.js
@@ -0,0 +1,4 @@
+import { start } from './src/server';
+
+const PORT = process.env.PORT || 3004;
+start(PORT);
--- index.ts
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-import { start } from './src/server';
-
-const PORT: number = parseInt(<string>process.env.PORT, 10) || 3004;
-
-start(PORT);
